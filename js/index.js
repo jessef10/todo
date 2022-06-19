d6b905a52ece046cb5242270381775563765a782
jesse <itsjessef@gmail.com>
2022-06-19 15:41:48 -0700

    save data to local storage

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
 // Model
-let todos = [{
-  title: 'Get groceries',
-  dueDate: '2021-10-02',
-  id: 'id1'
-}, {
-  title: 'Wash car',
-  dueDate: '2021-02-02',
-  id: 'id2'
-}, {
-  title: 'Make dinner',
-  dueDate: '2021-07-11',
-  id: 'id3'
-}];
+let todos;
 
+// Retrieve localStorage
+const savedTodos = JSON.parse(localStorage.getItem('todos'));
+if (Array.isArray(savedTodos)) {
+  todos = savedTodos;
+} else {
+  todos = [{
+    title: 'Get groceries',
+    dueDate: '2021-10-02',
+    id: 'id1'
+  }, {
+    title: 'Wash car',
+    dueDate: '2021-02-02',
+    id: 'id2'
+  }, {
+    title: 'Make dinner',
+    dueDate: '2021-07-11',
+    id: 'id3'
+  }];
+}
+
+// Creates an id
 function createId() {
   return '' + new Date().getTime();
 }
 
+// Creates a todo
 function createTodo(title, dueDate) {
   const id = createId();
 
@@ -25,8 +35,11 @@ function createTodo(title, dueDate) {
     dueDate: dueDate,
      id: id
   });
+
+  saveTodos();
 }
 
+// Deletes a todo
 function removeTodo(idToDelete) {
   todos = todos.filter(function (todo) {
     if (todo.id === idToDelete) {
@@ -35,14 +48,19 @@ function removeTodo(idToDelete) {
       return true;
     }
   });
+
+  saveTodos();
 }
 
-render();
+// Saves a todo
+function saveTodos() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
 
 // Controller
 function addTodo() {
-  let textbox = document.querySelector('#todo-title');
-  let title = textbox.value;
+  const textbox = document.querySelector('#todo-title');
+  const title = textbox.value;
 
   const datePicker = document.querySelector('#date-picker');
   const dueDate = datePicker.value;
@@ -78,5 +96,6 @@ function render() {
     let todoList = document.getElementById('todo-list');
     todoList.appendChild(element);
   });
-
 }
+
+render();
